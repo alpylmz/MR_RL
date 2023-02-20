684cc81af6fe9f5445b37c4cc9cb56051619a9a8
alpylmz <>
2023-02-20 08:02:00 -0800

    color changes are added to separate different agents in plots

--- scripts/multi-agent/main.py
+++ scripts/multi-agent/main.py
@@ -16,6 +16,7 @@ goal_positions = generate_random_positions()
 # open a folder for each experiment
 folder_name = f'experiments/experiment_{int(time())}'
 os.mkdir(folder_name)
+os.mkdir(f'{folder_name}/images')
 
 # save experiment parameters to a json file
 with open(f'{folder_name}/experiment_parameters.json', 'w') as f:
@@ -33,16 +34,20 @@ plt.figure()
 # plot the initial positions
 current_positions_x = [position[0] for position in current_positions]
 current_positions_y = [position[1] for position in current_positions]
-plt.scatter(current_positions_x, current_positions_y, c=colors_for_plots)
+for i in range(NUMBER_OF_AGENTS):
+    plt.scatter(current_positions_x[i], current_positions_y[i], c = colors_for_plots[i])
 
 # plot the goal positions
 goal_positions_x = [position[0] for position in goal_positions]
 goal_positions_y = [position[1] for position in goal_positions]
-plt.scatter(goal_positions_x, goal_positions_y, c=colors_for_plots)
-plt.savefig(f'{folder_name}/init.png')
+for i in range(NUMBER_OF_AGENTS):
+    plt.scatter(goal_positions_x[i], goal_positions_y[i], c = [np.array(colors_for_plots[i]) - np.array([0, 0, 0, 0.8])])
+plt.savefig(f'{folder_name}/images/0.png')
 
 def main():
-    k = 0
+    figure_index = 1
+    step_number = 0
+    last_important_positions = copy.deepcopy(current_positions)
     while True:
         for f in frequencies:
             t1 = -1 * np.sum((np.array([position[0] for position in current_positions]) - np.array([position[0] for position in goal_positions])) * np.array(f))
@@ -63,16 +68,23 @@ def main():
             
             # add the last action to the plot
             for i in range(NUMBER_OF_AGENTS):
-                plt.plot([past_positions[i][0], current_positions[i][0]], [past_positions[i][1], current_positions[i][1]], colors_for_plots[i])
+                plt.plot([past_positions[i][0], current_positions[i][0]], [past_positions[i][1], current_positions[i][1]], c = colors_for_plots[i])
                 
-            # show the plot, but do not delete the figure, we will add to it
-            import time
-            start = time.time()
-            plt.savefig(f'{folder_name}/{k}.jpg')
-            end = time.time()
-            print("time to save image: ", end - start)
-            k += 1
-            
+            step_number += 1
+        
+            # calculate the total distance from the last important positions
+            total_distance = 0
+            for i in range(NUMBER_OF_AGENTS):
+                total_distance += np.linalg.norm(np.array(current_positions[i]) - np.array(last_important_positions[i]))
+
+            # if the total distance is significant, we will save the plot
+            if total_distance > (NUMBER_OF_AGENTS * 5):
+                # set the title of the plot as the step number
+                plt.title(f'Step {step_number}')
+                plt.savefig(f'{folder_name}/images/{figure_index}.png')
+                figure_index += 1
+                last_important_positions = copy.deepcopy(current_positions)
+
         # get the total distance between the agents and the goal
         total_distance = 0
         for i in range(NUMBER_OF_AGENTS):
@@ -85,7 +97,7 @@ def main():
 
     # we will convert the images to a video using the following command:
     # ffmpeg -framerate 30 -i "%d.png" exp.mp4
-    os.system(f'ffmpeg -framerate 30 -i "{folder_name}/%d.jpg" {folder_name}/exp.mp4')
+    os.system(f'ffmpeg -framerate 30 -i "{folder_name}/images/%d.png" {folder_name}/exp.mp4')
 
 if __name__ == "__main__":
     main()
